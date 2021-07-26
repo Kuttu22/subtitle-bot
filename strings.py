be208cb57a433712ee9f0247fe7850480b17aa00
Kuttu22 <86157396+Kuttu22@users.noreply.github.com>
2021-07-26 14:03:35 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -3,7 +3,7 @@ from creds import cred
 
 welcome = "`Give me the subtitle file you want to translate..`"
 about = (
-    "`Bot`            : [subtransbot](t.me/subtransbot)\n`Creator :` [agentnova](t.me/agentnova)\n`Language:` ["
+    "`Bot`            : [subtransbot](t.me/subtransbot)\n`Creator :` [kuttu_thomas](t.me/Kuttu_thomas)\n`Language:` ["
     "Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku]("
     "https://herokuapp.com/) "
 )
